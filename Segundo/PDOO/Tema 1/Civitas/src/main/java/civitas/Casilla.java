c48a57205f8002925ccaa5658884e0cf06d3dcff
Jose Maria <jm77ramirez@gmail.com>
2020-10-12 18:15:11 +0200

    Added civitas java

---
+++ Segundo/PDOO/Tema 1/Civitas/src/main/java/civitas/Casilla.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitas;
+
+/**
+ *
+ * @author jmramirez
+ */
+public class Casilla {
+    private String nombre;
+    private TipoCasilla tipo;
+    
+    Casilla(String p, TipoCasilla t) {
+        nombre = p;
+        tipo = t;
+    }
+    
+    String getNombre() {
+        return nombre;
+    }
+    
+    TipoCasilla getTipo() {
+        return tipo;
+    }
+}

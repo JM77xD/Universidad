40a9d92b8257c11e093225576916bf4e1c48ebaf
jmramirezG <jm77ramirez@gmail.com>
2021-05-28 21:04:46 +0200

    Añadido AlfaBeta, mejorar heurística

--- Segundo/IA/P3/Conecta4Boom_linux/player.cpp
+++ Segundo/IA/P3/Conecta4Boom_linux/player.cpp
@@ -53,9 +53,117 @@ double ValoracionTest(const Environment &estado, int jugador){
 
 // ------------------- Los tres metodos anteriores no se pueden modificar
 
+/**
+ * @brief Cuenta las fichas sueltas pertenecientes a un jugador en el estado dado
+ * 
+ * @param jugador Jugador del que contar las parejas
+ * @param estado Estado en el que contar las parejas
+ * @return Devuelve el número de fichas sueltas del jugador
+ */
+int contarIndiv(int jugador, const Environment &estado) {
+   int individuales = 0;
+   
+   for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 7; j++) {
+         if (estado.See_Casilla(i, j) == jugador)
+            individuales++;
+      }
+   }
+
+   return individuales;
+}
+
+/**
+ * @brief Cuenta las parejas pertenecientes a un jugador en el estado dado
+ * 
+ * @param jugador Jugador del que contar las parejas
+ * @param estado Estado en el que contar las parejas
+ * @return Devuelve el número de parejas
+ */
+int contarParejas(int jugador, const Environment &estado) {
+   int parejas = 0;
+
+   for (int i = 0; i < 6; i++) {
+      for (int j = 0; j < 6; j++) {
+
+         if (estado.See_Casilla(i,j)==jugador && estado.See_Casilla(i,j+1)==jugador)
+            parejas++;
+
+         if (estado.See_Casilla(i,j)==jugador && estado.See_Casilla(i+1,j+1)==jugador)
+            parejas++;
+         
+         if (estado.See_Casilla(i,j)==jugador && estado.See_Casilla(i+1,j)==jugador)
+            parejas++;
+
+      }
+   }
+
+   return parejas;
+}
+
+/**
+ * @brief Cuenta los trios pertenecientes a un jugador en el estado dado
+ * 
+ * @param jugador Jugador del que contar las parejas
+ * @param estado Estado en el que contar las parejas
+ * @return Devuelve una pareja con el número de trios y los números de columna y fila en los que están
+ */
+int contarTrios(int jugador, const Environment &estado) {
+
+   int trios = 0;
+
+   for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+
+         if (estado.See_Casilla(i,j)==jugador && estado.See_Casilla(i,j+1)==jugador && estado.See_Casilla(i,j+2)==jugador)
+            trios++;
+
+         if (estado.See_Casilla(i,j)==jugador && estado.See_Casilla(i+1,j+1)==jugador && estado.See_Casilla(i+2,j+2)==jugador)
+            trios++;
+         
+         if (estado.See_Casilla(i,j)==jugador && estado.See_Casilla(i+1,j)==jugador && estado.See_Casilla(i+2,j)==jugador)
+            trios++;
+      }
+   }
+
+   return trios;
+   
+}
+
 // Función que calcula la heurística de un jugador para un estado
 double Heuristica(int jugador, const Environment &estado) {
 
+   double suma = 0;
+
+   //Contamos los trios
+
+   suma += contarTrios(jugador, estado) * 10000;
+
+
+   suma -= contarTrios(jugador==1?2:1, estado);
+
+
+   //Contamos las parejas
+
+   suma += contarParejas(jugador, estado) * 1000;
+
+   suma -= contarParejas(jugador==1?2:1, estado) * 1000;
+
+   
+   //Contamos los individuales
+   suma += contarIndiv(jugador, estado)*100;
+
+   suma -= contarIndiv(jugador==1?2:1, estado)*100;
+
+   Environment::ActionType acc = static_cast< Environment::ActionType > (estado.Last_Action(jugador));
+
+   if (acc == Environment::PUT4)
+      suma += 500;
+   else if (acc == Environment::PUT3 || acc == Environment::PUT5)
+      suma += 250;
+   
+   
+   return suma; 
 }
 
 
@@ -65,13 +173,52 @@ double Valoracion(const Environment &estado, int jugador){
    int ganador = estado.RevisarTablero();
 
     if (ganador==jugador)
-       return 99999999.0; // Gana el jugador que pide la valoracion
+      return 99999999.0; // Gana el jugador que pide la valoracion
     else if (ganador!=0)
-            return -99999999.0; // Pierde el jugador que pide la valoracion
+      return -99999999.0; // Pierde el jugador que pide la valoracion
     else if (estado.Get_Casillas_Libres()==0)
-            return 0;  // Hay un empate global y se ha rellenado completamente el tablero
+      return 0;  // Hay un empate global y se ha rellenado completamente el tablero
     else
-          return Heuristica(jugador,estado);
+      return Heuristica(jugador,estado);
+}
+
+double Poda_AlfaBeta(const Environment &estado, const int Profundidad, double alfa, double beta, int jugadorActual, const int JugadorMax, const int rep = 1) {
+   int fin = estado.RevisarTablero();
+   if (Profundidad == 0 || estado.Get_Casillas_Libres() == 0 || fin == 0)
+      return Valoracion(estado, jugadorActual);
+
+   Environment next[10];
+   int tam = estado.GenerateAllMoves(next);
+
+   if (jugadorActual == JugadorMax) {
+
+      for (int i = 0; i < tam; i++) {
+         if (rep == 1)
+            beta = min(beta, Poda_AlfaBeta(next[i], Profundidad - 1, alfa, beta, jugadorActual, JugadorMax, 2));
+         else
+            alfa = max(alfa, Poda_AlfaBeta(next[i], Profundidad - 1, alfa, beta, next[i].JugadorActivo(), JugadorMax));
+
+         if (beta <= alfa)
+            break;
+      }
+
+      return alfa;
+
+   } else {
+
+      for (int i = 0; i < tam; i++) {
+         if (rep == 1)
+            beta = min(beta, Poda_AlfaBeta(next[i], Profundidad - 1, alfa, beta, jugadorActual, JugadorMax, 2));
+         else
+            alfa = max(alfa, Poda_AlfaBeta(next[i], Profundidad - 1, alfa, beta, next[i].JugadorActivo(), JugadorMax));
+
+         if (beta <= alfa)
+            break;
+      }
+
+      return beta;
+
+   }
 }
 
 
@@ -114,8 +261,8 @@ Environment::ActionType Player::Think(){
 
 
 
-    double valor; // Almacena el valor con el que se etiqueta el estado tras el proceso de busqueda.
-    double alpha, beta; // Cotas de la poda AlfaBeta
+    double valor = -99999999.0, aux; // Almacena el valor con el que se etiqueta el estado tras el proceso de busqueda.
+    double alpha = -99999999.0, beta = 99999999.0; // Cotas de la poda AlfaBeta
 
     int n_act; //Acciones posibles en el estado actual
 
@@ -134,6 +281,7 @@ Environment::ActionType Player::Think(){
 
 
     //--------------------- COMENTAR Desde aqui
+    /*
     cout << "\n\t";
     int n_opciones=0;
     JuegoAleatorio(aplicables, opciones, n_opciones);
@@ -156,6 +304,7 @@ Environment::ActionType Player::Think(){
             accion = static_cast< Environment::ActionType > (opciones[aleatorio]);
          }
 
+    */
     //--------------------- COMENTAR Hasta aqui
 
 
@@ -164,8 +313,20 @@ Environment::ActionType Player::Think(){
 
     // Opcion: Poda AlfaBeta
     // NOTA: La parametrizacion es solo orientativa
-    // valor = Poda_AlfaBeta(actual_, jugador_, 0, PROFUNDIDAD_ALFABETA, accion, alpha, beta);
-    //cout << "Valor MiniMax: " << valor << "  Accion: " << actual_.ActionStr(accion) << endl;
+      Environment next[8];
+      int tam = actual_.GenerateAllMoves(next);
+      for (int i = 0; i < tam; i++) {
+         if (next[i].JugadorActivo() == jugador_)
+            aux = Poda_AlfaBeta(next[i], PROFUNDIDAD_ALFABETA, alpha, beta, jugador_ , jugador_, 2);
+         else
+            aux = Poda_AlfaBeta(next[i], PROFUNDIDAD_ALFABETA, alpha, beta, next[i].JugadorActivo() , jugador_, 1);
+
+         if (aux >= valor) {
+            valor = aux;
+            accion = static_cast< Environment::ActionType > (next[i].Last_Action(jugador_));
+         }
+      }
+    cout << "Valor MiniMax: " << valor << "  Accion: " << actual_.ActionStr(accion) << endl;
 
     return accion;
 }

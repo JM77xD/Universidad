30b32f5c4870bdc9e279b7be8889b3f5210ca546
Jose Maria Ramirez Gonzalez <jm.ramirez.gonza@gmail.com>
2022-03-28 21:32:58 +0200

    P1 MH, falta informe

--- Tercero/MH/P1/P1_JoseMariaRamirezGonzalez.py
+++ Tercero/MH/P1/P1_JoseMariaRamirezGonzalez.py
@@ -1,7 +1,7 @@
 import os
 import time
 import numpy as np
-from random import randint, shuffle
+from random import randint, shuffle, seed
 
 def sortingKey(e):
     first_digit = int(e[6])
@@ -27,166 +27,179 @@ data_dir = 'datos/'
 data_filenames = os.listdir(data_dir)
 
 data_filenames.sort(key=sortingKey)
+seeds = [19102001,20102001,19112001,19102002,21112001]
+writeOn = open("results.txt", '+w')
 
 for filename in data_filenames: # Para cada archivo de datos
-    file = open(data_dir+filename)
 
+    mean_time_greedy = 0
+    mean_time_bl = 0
+    mean_value_greedy = 0
+    mean_value_bl = 0
 
-    first_line = file.readline().split(' ')
-
-    n, m = int(first_line[0]), int(first_line[1])   # Leemos numero de elementos y objetivo
-
-    data_matrix = np.zeros((n,n))   # Creamos la matriz de entradas
-    possible_options = []
-
-    for line in file:
-        line = line.strip('\n').split(' ')
-        possible_options.append([int(line[0]), int(line[1])])
-        data_matrix[int(line[0]), int(line[1])] = float(line[2])
-        data_matrix[int(line[1]), int(line[0])] = float(line[2])
-
-    start_time_greedy = time.process_time()
+    for l in range(5):
+        seed(seeds[l])
     
-    first_element = possible_options[randint(0,n-1)]
-    result_elements = [first_element[0], first_element[1]]
+        file = open(data_dir+filename)
 
-    for i in range(m-2):
-        min_value = float('inf')
-        curr_index = None
+
+        first_line = file.readline().split(' ')
+
+        n, m = int(first_line[0]), int(first_line[1])   # Leemos numero de elementos y objetivo
+
+        data_matrix = np.zeros((n,n))   # Creamos la matriz de entradas
+        possible_options = []
+
+        for line in file:
+            line = line.strip('\n').split(' ')
+            possible_options.append([int(line[0]), int(line[1])])
+            data_matrix[int(line[0]), int(line[1])] = float(line[2])
+            data_matrix[int(line[1]), int(line[0])] = float(line[2])
+        
+        file.close()
+
+        start_time_greedy = time.process_time()
+        
+        first_element = possible_options[randint(0,n-1)]
+        result_elements = [first_element[0], first_element[1]]
+
+        for i in range(m-2):
+            min_value = float('inf')
+            curr_index = None
+
+            delta_values = []
+
+            for element in result_elements:
+                delta_value = 0
+
+                for other_elem in result_elements:
+                    delta_value += data_matrix[element, other_elem]
+                        
+                delta_values.append(delta_value)
+
+            for k in range(n):
+                if k not in result_elements:
+
+                    possible_result = result_elements.copy()
+
+                    possible_result.append(k)
+
+                    possible_delta = delta_values.copy()
+
+                    for values, element in zip(possible_delta,result_elements):
+                        values += data_matrix[k,element]
+                    
+                    delta_value = 0
+
+                    for element in possible_result:
+                        delta_value += data_matrix[k,element]
+
+                    possible_delta.append(delta_value)
+
+                    possible_delta.sort()
+
+                    actual_dif = possible_delta[-1] - possible_delta[0]
+
+                    if actual_dif < min_value:
+                        min_value = actual_dif
+                        curr_index = k
+            
+            result_elements.append(curr_index)
+        
 
         delta_values = []
 
         for element in result_elements:
             delta_value = 0
-
             for other_elem in result_elements:
-                delta_value += data_matrix[element, other_elem]
-                    
+                if other_elem != element:
+                    delta_value += data_matrix[element, other_elem]
             delta_values.append(delta_value)
-
-        for k in range(n):
-            if k not in result_elements:
-
-                possible_result = result_elements.copy()
-
-                possible_result.append(k)
-
-                possible_delta = delta_values.copy()
-
-                for values, element in zip(possible_delta,result_elements):
-                    values += data_matrix[k,element]
-                
-                delta_value = 0
-
-                for element in possible_result:
-                    delta_value += data_matrix[k,element]
-
-                possible_delta.append(delta_value)
-
-                possible_delta.sort()
-
-                actual_dif = possible_delta[-1] - possible_delta[0]
-
-                if actual_dif < min_value:
-                    min_value = actual_dif
-                    curr_index = k
         
-        result_elements.append(curr_index)
-    
+        delta_values.sort()
 
-    delta_values = []
+        end_time_greedy = time.process_time()
 
-    for element in result_elements:
-        delta_value = 0
-        for other_elem in result_elements:
-            if other_elem != element:
-                delta_value += data_matrix[element, other_elem]
-        delta_values.append(delta_value)
-    
-    delta_values.sort()
+        mean_time_greedy += (end_time_greedy-start_time_greedy)/5
+        mean_value_greedy += (delta_values[-1]-delta_values[0])/5
 
-    end_time_greedy = time.process_time()
 
-    print("-------------------------------------GREEDY--------------------------------------")
-    print("Coste de la solución de {}: {}\nTiempo de ejecución: {}\n\n".format(filename, delta_values[-1]-delta_values[0], end_time_greedy-start_time_greedy))
+        possible_options = [i for i in range(n)]
+        start_time_BL = time.process_time()
 
-    possible_options = [i for i in range(n)]
-    start_time_BL = time.process_time()
+        shuffle(possible_options)
 
-    shuffle(possible_options)
+        Sel = possible_options[0:m].copy()
+        Sel_costs = []
 
-    Sel = possible_options[0:m].copy()
-    Sel_costs = []
+        for element in Sel:
+            cost = 0
+            for other_elem in Sel:
+                cost += data_matrix[element, other_elem]
+            Sel_costs.append(cost)
+            possible_options.remove(element)
 
-    for element in Sel:
-        cost = 0
-        for other_elem in Sel:
-            cost += data_matrix[element, other_elem]
-        Sel_costs.append(cost)
-        possible_options.remove(element)
+        changing = True
 
-    changing = True
+        evaluations = 0
 
-    evaluations = 0
+        while changing and evaluations < 100000:
+            changing = False
 
-    while changing and evaluations < 100000:
-        changing = False
+            merged = list(zip(Sel, Sel_costs))
+            merged = sorted(merged, key=sortingKey_Sel)
+            Sel = [element[0] for element in merged]
+            Sel_costs = [element[1] for element in merged]
+
+
+            for u in Sel:
+                for v in possible_options:
+                    index = Sel.index(u)
+                    newSel = Sel.copy()
+                    newSel[index] = v
+
+                    newSel_costs = [elem - data_matrix[u, item] + data_matrix[v, item] for elem, item in zip(Sel_costs, Sel)]
+                    newSel_costs.pop(index)
+                    value = 0
+                    for elem in newSel:
+                        value += data_matrix[v, elem]
+                    
+                    newSel_costs.insert(index, value)
+
+
+                    new_merged = list(zip(newSel, newSel_costs))
+                    new_merged = sorted(new_merged, key=sortingKey_Sel)
+                    newSel = [element[0] for element in new_merged]
+                    newSel_costs = [element[1] for element in new_merged]
+
+
+                    if (newSel_costs[-1] - newSel_costs[0]) < (Sel_costs[-1] - Sel_costs[0]):
+                        possible_options.append(u)
+                        possible_options.remove(v)
+                        changing = True
+                        Sel = newSel.copy()
+                        Sel_costs = newSel_costs.copy()
+                        break
+
+                    evaluations += 1
+
+                    if evaluations >= 100000:
+                        break
+                    
+                if changing or evaluations >= 100000:
+                    break
+        
 
         merged = list(zip(Sel, Sel_costs))
         merged = sorted(merged, key=sortingKey_Sel)
         Sel = [element[0] for element in merged]
         Sel_costs = [element[1] for element in merged]
-
-
-        for u in Sel:
-            for v in possible_options:
-                index = Sel.index(u)
-                newSel = Sel.copy()
-                newSel[index] = v
-
-                newSel_costs = [elem - data_matrix[u, item] + data_matrix[v, item] for elem, item in zip(Sel_costs, Sel)]
-                newSel_costs.
-                value = 0
-                for elem in newSel:
-                    value += data_matrix[v, elem]
-                
-                newSel_costs.insert(index, value)
-
-
-                new_merged = list(zip(newSel, newSel_costs))
-                new_merged = sorted(new_merged, key=sortingKey_Sel)
-                newSel = [element[0] for element in new_merged]
-                newSel_costs = [element[1] for element in new_merged]
-
-
-                if (newSel_costs[-1] - newSel_costs[0]) < (Sel_costs[-1] - Sel_costs[0]):
-                    possible_options.append(u)
-                    possible_options.remove(v)
-                    changing = True
-                    Sel = newSel.copy()
-                    Sel_costs = newSel_costs.copy()
-                    break
-
-                evaluations += 1
-
-                if evaluations >= 100000:
-                    break
-                
-            if changing or evaluations >= 100000:
-                break
-    
-
-    merged = list(zip(Sel, Sel_costs))
-    merged = sorted(merged, key=sortingKey_Sel)
-    Sel = [element[0] for element in merged]
-    Sel_costs = [element[1] for element in merged]
-    
-    end_time_BL = time.process_time()
         
-    
-    print("--------------------------------------BL----------------------------------------")
-    print("Coste de la solución de {}: {}\nTiempo de ejecución: {}\nEvaluaciones: {}\n\n".format(filename, Sel_costs[-1]-Sel_costs[0], end_time_BL-start_time_BL, evaluations))
+        end_time_BL = time.process_time()
 
+        mean_time_bl += (end_time_BL-start_time_BL)/5
+        mean_value_bl += (Sel_costs[-1]-Sel_costs[0])/5
+            
 
-
-
+    print("\n\n--------------------------Valores medios {}--------------------------\nTiempo, valor:\n\tGreedy: {}, {}\n\tBL: {}, {}\n".format(filename, mean_time_greedy, mean_value_greedy, mean_time_bl, mean_value_bl))
+    writeOn.write("{} {} {} {}\n".format(mean_value_greedy, mean_time_greedy, mean_value_bl, mean_time_bl))
